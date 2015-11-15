ff660d023fb31af15e10baa3aadfc91a20a599db
解玉坤 <realxie@jieyukundeMacBook-Pro.local>
2015-11-15 16:30:53 +0800

    update...

--- PPNode.m
+++ PPNode.m
@@ -11,37 +11,63 @@
 #import <OpenGLES/ES2/glext.h>
 #import <GLKit/GLKit.h>
 
-GLfloat data[] = {
-    -1.0f, -1.0f, 1.0f, -0.577350, -0.577350, 0.577350, 0.0, 0.0,
-    -1.0f, 1.0f, 1.0f, -0.577350, 0.577350, 0.577350,0.0, 1.0,
-    1.0f, 1.0f, 1.0f, 0.577350, 0.577350, 0.577350, 1.0, 0.0,
-    1.0f, -1.0f, 1.0f, 0.577350, -0.577350, 0.577350, 1.0, 1.0,
+typedef struct {
+    GLfloat position[3];
+    GLfloat color[3];
+    GLfloat uv[2];
+} Vertex;
+
+Vertex data[] = {
+    {{-1.0f, -1.0f, 1.0f}, {-0.577350, -0.577350, 0.577350}, {0.0, 0.0}},
+    {{-1.0f, 1.0f, 1.0f}, {-0.577350, 0.577350, 0.577350},{0.0, 1.0}},
+    {{1.0f, 1.0f, 1.0f}, {0.577350, 0.577350, 0.577350}, {1.0, 0.0}},
+    {{1.0f, -1.0f, 1.0f}, {0.577350, -0.577350, 0.577350}, {1.0, 1.0}},
     
-    1.0f, -1.0f, -1.0f, 0.577350, -0.577350, -0.577350, 0.0, 1.0,
-    1.0f, 1.0f, -1.0f, 0.577350, 0.577350, -0.577350, 1.0, 0.0,
-    -1.0f, 1.0f, -1.0f, -0.577350, 0.577350, -0.577350, 0.0, 0.0,
-    -1.0f, -1.0f, -1.0f, -0.577350, -0.577350, -0.577350, 1.0, 1.0
+    {{1.0f, -1.0f, -1.0f}, {0.577350, -0.577350, -0.577350}, {0.0, 1.0}},
+    {{1.0f, 1.0f, -1.0f}, {0.577350, 0.577350, -0.577350}, {1.0, 0.0}},
+    {{-1.0f, 1.0f, -1.0f}, {-0.577350, 0.577350, -0.577350}, {0.0, 0.0}},
+    {{-1.0f, -1.0f, -1.0f}, {-0.577350, -0.577350, -0.577350}, {1.0, 1.0}},
     
+    // right face
+    {{1.0f, 1.0f, 1.0f}, {0.577350, 0.577350, 0.577350}, {0.0, 1.0}},
+    {{1.0f, -1.0f, 1.0f}, {0.577350, -0.577350, 0.577350}, {0.0, 0.0}},
+    {{1.0f, -1.0f, -1.0f}, {0.577350, -0.577350, -0.577350}, {1.0, 0.0}},
+    {{1.0f, 1.0f, -1.0f}, {0.577350, 0.577350, -0.577350}, {1.0, 1.0}}
     
 };
+
+
+//GLfloat data[] = {
+//    -1.0f, -1.0f, 1.0f, -0.577350, -0.577350, 0.577350, 0.0, 0.0,
+//    -1.0f, 1.0f, 1.0f, -0.577350, 0.577350, 0.577350,0.0, 1.0,
+//    1.0f, 1.0f, 1.0f, 0.577350, 0.577350, 0.577350, 1.0, 0.0,
+//    1.0f, -1.0f, 1.0f, 0.577350, -0.577350, 0.577350, 1.0, 1.0,
+//    
+//    1.0f, -1.0f, -1.0f, 0.577350, -0.577350, -0.577350, 0.0, 1.0,
+//    1.0f, 1.0f, -1.0f, 0.577350, 0.577350, -0.577350, 1.0, 0.0,
+//    -1.0f, 1.0f, -1.0f, -0.577350, 0.577350, -0.577350, 0.0, 0.0,
+//    -1.0f, -1.0f, -1.0f, -0.577350, -0.577350, -0.577350, 1.0, 1.0,
+//    
+//    // right face
+//    1.0f, 1.0f, 1.0f, 0.577350, 0.577350, 0.577350, 0.0, 1.0,
+//    1.0f, -1.0f, 1.0f, 0.577350, -0.577350, 0.577350, 0.0, 0.0,
+//    1.0f, -1.0f, -1.0f, 0.577350, -0.577350, -0.577350, 1.0, 0.0,
+//    1.0f, 1.0f, -1.0f, 0.577350, 0.577350, -0.577350, 1.0, 1.0
+//    
+//};
 
 
 GLushort indices[]={
     // Front face
     3, 2, 1, 3, 1, 0,
-    
     // Back face
     7, 5, 4, 7, 6, 5,
-    
     // Left face
     0, 1, 7, 7, 1, 6,
-    
     // Right face
-    3, 4, 5, 3, 5, 2,
-    
+    8, 9, 10, 8, 10, 11,
     // Up face
     1, 2, 5, 1, 5, 6,
-    
     // Down face
     0, 7, 3, 3, 7, 4
 };
@@ -61,13 +87,13 @@ GLushort indices[]={
         glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
         
         glEnableVertexAttribArray(GLKVertexAttribPosition);
-        glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 32, BUFFER_OFFSET(0));
+        glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 32, (GLvoid*)offsetof(Vertex, position));
         
         glEnableVertexAttribArray(GLKVertexAttribColor);
-        glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, 32, BUFFER_OFFSET(12));
+        glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, 32, (GLvoid*)offsetof(Vertex, color));
         
         glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-        glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_TRUE, 32, BUFFER_OFFSET(24));
+        glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_TRUE, 32, (GLvoid*)offsetof(Vertex, uv));
         
     }
     return self;
